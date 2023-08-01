26e6b30a65a27b0475a4bc2e58d2ac6cbb1e1b2b
gvarys <johnnyzaev@gmail.com>
2023-08-01 12:31:24 +0400

    Added spaceship movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,41 @@ using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class PlayerMovement : MonoBehaviour
 {
+	[SerializeField] private float forceMagnitude = 50f;
+	[SerializeField] private float maxVelocity = 6f;
+
 	private Camera _mainCamera;
+	private Rigidbody _playerRigidbody;
+	private Vector3 _movementDirection;
 
 	private void Awake()
 	{
 		_mainCamera = Camera.main;
+		_playerRigidbody = GetComponent<Rigidbody>();
 	}
 
 	private void Update()
 	{
 		if (Touch.activeTouches.Count > 0)
 		{
-			Debug.Log(_mainCamera.ScreenToWorldPoint(Touch.activeTouches[0].screenPosition));
+			var worldPoint = _mainCamera.ScreenToWorldPoint(Touch.activeTouches[0].screenPosition);
+
+			_movementDirection = transform.position - worldPoint;
+			_movementDirection.z = 0;
+			_movementDirection.Normalize();
 		}
+		else
+		{
+			_movementDirection = Vector3.zero;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (_movementDirection == Vector3.zero)
+			return;
+		
+		_playerRigidbody.AddForce(_movementDirection * (forceMagnitude * Time.fixedDeltaTime), ForceMode.Force);
+		_playerRigidbody.velocity = Vector3.ClampMagnitude(_playerRigidbody.velocity, maxVelocity);
 	}
 }
